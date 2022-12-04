923fa816ad06aacaf570a4373e7653767df8f8f7
ann <ann@adimian.com>
2022-12-04 23:15:49 +0100

    less loops

--- adv/day4.py
+++ adv/day4.py
@@ -20,6 +20,16 @@ def are_overlapping(A: tuple[int, int], B: tuple[int, int]) -> bool:
     return not not set(range(*A)).intersection(range(*B))
 
 
+def determine_kind_of_overlap(A: tuple[int, int], B: tuple[int, int]) -> str:
+    intersection = set(range(*A)).intersection(range(*B))
+    if (set(range(*A)) == intersection) or (set(range(*B)) == intersection):
+        return "A or B completely in intersection"
+    elif not not set(range(*A)).intersection(range(*B)):
+        return "some overlap"
+    else:
+        return "no overlap"
+
+
 with open(input, "r") as file:
     overlaps_completely = []
     overlaps = []
@@ -27,9 +37,12 @@ with open(input, "r") as file:
         section_A, section_B = line.strip().split(",")
         rangeA = pick_start_and_stop(section_A)
         rangeB = pick_start_and_stop(section_B)
-        if is_range_completely_in_the_other(rangeA, rangeB):
+        overlap = determine_kind_of_overlap(rangeA, rangeB)
+        if overlap == "A or B completely in intersection":
             overlaps_completely.append(i)
-        if are_overlapping(rangeA, rangeB):
+        elif overlap == "some overlap":
             overlaps.append(i)
+        else:
+            continue
     print(len(overlaps_completely))
-    print(len(overlaps))
+    print(len(overlaps + overlaps_completely))
