27d51b86341bea3dee5045e10c3524f2ef86c2ed
ann <ann@adimian.com>
2022-12-08 21:42:32 +0100

    solution part 1 of day 5

--- adv/day5.py
+++ adv/day5.py
@@ -32,12 +32,13 @@ from pathlib import Path
 data_folder = Path(__file__).parent.parent / "data"
 input = data_folder / "day5_input.txt"
 
+fake_input = data_folder / "day5_fake.txt"
 
 with open(input, "r") as file:
     stacks = []
     location_of_s = [
-        x for x in range(1, 36, 4)
-    ]  # the 9 stacks are located here
+        x for x in range(1, 36, 4)  # the 9 stacks are located here
+    ]
 
     for i, line in enumerate(file):
         if i < 8:  # stack data
@@ -45,19 +46,21 @@ with open(input, "r") as file:
                 box = "{:<36}".format(line.strip())[location_of_s[s]]
                 if i == 0:
                     stacks.append([])
-                stacks[s].append(box)
+                if box.isalpha():
+                    stacks[s].append(box)
         if i > 9:  # move data
-            how_many_text, what_stacks_text = line.strip().split("from ")
-            how_many_boxes = int(how_many_text.strip("move "))
-            from_stack, to_stack = what_stacks_text.split(" to")
+            how_many_as_text, what_stacks_as_text = line.strip().split("from ")
+            how_many_boxes = int(how_many_as_text.strip("move "))
+            from_stack, to_stack = what_stacks_as_text.split(" to")
             from_stack, to_stack = int(from_stack) - 1, int(to_stack) - 1
             for box in range(how_many_boxes):
                 box_on_crane = stacks[from_stack].pop(0)
                 stacks[to_stack].insert(0, box_on_crane)
 
-top = []
+top_boxes = []
 for stack in stacks:
-    print(stack)
     top_box = stack.pop(0)
-    top.append(top_box)
-print("".join(top))
+    top_boxes.append(top_box)
+print("".join(top_boxes))
+
+# is TPGVQPFDH
